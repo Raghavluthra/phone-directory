51d490b3870d27ff196fb6150f179a245a278674
Your Name <you@example.com>
2019-12-09 13:32:04 +0530

    JSX: Displayed 'Phone Directory'

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React, { Component } from 'react';
 
 
 class App extends Component{
+  render() {
   return (
-   <div>
 
-   </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+       Phone Directory
+       </div>
   );
 }
+};
 
 export default App;
